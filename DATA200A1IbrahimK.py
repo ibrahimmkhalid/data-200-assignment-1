a5d59d2e0ea89f8a8639d51273605d76a5a0ef65
Ibrahim Mansoor Khalid <ibrahimmkhalid@gmail.com>
2023-10-01 17:46:45 -0700

    update

--- DATA200A1IbrahimK.py
+++ DATA200A1IbrahimK.py
@@ -3,14 +3,55 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import streamlit as st
 
+class AutoSidebar():
+    def __init__(self):
+        self.__sidebar = []
+
+    def header(self, text, tag):
+        st.header(text, tag)
+        self.__sidebar.append({"type": "header", "text": f"[{text}](#{tag})"})
+
+    def subheader(self, text, tag):
+        st.subheader(text, tag)
+        self.__sidebar.append({"type": "subheader", "text": f"[{text}](#{tag})"})
+
+    def title(self, text, tag):
+        st.title(text, tag)
+        self.__sidebar.append({"type": "title", "text": f"[{text}](#{tag})"})
+
+    def make(self):
+        for s in self.__sidebar:
+            if s["type"] == "header":
+                st.sidebar.header(s["text"])
+            elif s["type"] == "subheader":
+                st.sidebar.subheader("> "+s["text"])
+            else:
+                st.sidebar.title(s["text"])
+
+sidebar = AutoSidebar()
+
 df = pd.read_csv("./Fish.csv")
-st.header("Some fishy data")
+sidebar.title("Some fishy data", "home")
 st.write("The following are some interesting graphs related to a fish dataset")
-st.write("Here is a sample of the dataset:")
-st.write(df.head())
+st.write("To get started, lets take a look at the kind of dataset we are looking at")
+st.write("Here is the dataset in full, including some of its statistics")
+st.write(df)
+st.write(df.describe())
+
+data_bar = df["Species"].value_counts()
+data_pie = data_bar.to_dict()
 
 st.markdown("---")
-data_bar = df["Species"].value_counts()
+sidebar.header("Populations", "populations")
+sidebar.subheader("Pie chart", "pie_chart")
+st.write("Proportions of all the species present in the database")
+fig, ax = plt.subplots()
+plt.pie(data_pie.values(), labels=data_pie.keys())
+plt.title("Pie chart of number of fish species")
+st.pyplot(fig)
+
+sidebar.subheader("Bar chart", "bar_chart")
+st.write("Here is the same data as a bar chart")
 fig, ax = plt.subplots()
 ax.bar(data_bar.index, data_bar)
 plt.title("Fish count by species")
@@ -19,15 +60,18 @@ plt.ylabel("Count")
 plt.xticks(rotation=45)
 st.pyplot(fig)
 
-st.markdown("---")
-data_pie = df["Species"].value_counts().to_dict()
-fig, ax = plt.subplots()
-plt.pie(data_pie.values(), labels=data_pie.keys())
-plt.legend(loc='upper center', bbox_to_anchor=(1.2, 1))
-plt.title("Pie chart of number of fish species")
-st.pyplot(fig)
+data_bar_names = data_bar.keys()
+most_common_name = data_bar_names[0]
+most_common_count = data_bar[0]
+second_name = data_bar_names[1]
+least_name = data_bar_names[-1]
+least_count = data_bar[-1]
+st.write("""As we can see from the above, the {} species is the most common, with a total of {} specimen, then followed by {}. {} has the least 
+         number of specimen at {} members.""".format(most_common_name, most_common_count, second_name, least_name, least_count))
 
 st.markdown("---")
+sidebar.header("Scatter plot", "scatter")
+st.write("Next, lets take a look at the weight and widths of all the Pikes")
 data_scatter_x = df.loc[df["Species"] == "Pike", "Weight"]
 data_scatter_y = df.loc[df["Species"] == "Pike", "Width"]
 fig, ax = plt.subplots()
@@ -38,6 +82,7 @@ plt.title("Scatter plot of weight vs width for all pike fish")
 st.pyplot(fig)
 
 st.markdown("---")
+sidebar.header("Statistical view of all fish widths", "box_plots")
 data_box = {species : df.loc[df["Species"] == species, "Width"].tolist() for species in df["Species"].unique()}
 data_box_labels = list(data_box.keys())
 data_box_values = list(data_box.values())
@@ -49,8 +94,10 @@ plt.title("Box plot of the weights of each type of fish")
 st.pyplot(fig)
 
 st.markdown("---")
+sidebar.header("Interactive fish lengths", "lengths")
 species = df["Species"].unique()
-selected_species = st.selectbox("Get the lengths of the selected species over the course of 3 weeks (will be limited to the first 5 fish of the selected species)",species)
+st.write("Get the lengths of the selected species over the course of 3 weeks (will be limited to the first 5 fish of the selected species)")
+selected_species = st.selectbox("select", species)
 data_line = df.loc[df["Species"] == selected_species, ["Length1", "Length2", "Length3"]]
 fig, ax = plt.subplots()
 count = 0
@@ -68,3 +115,5 @@ plt.title("Length of all {} over period of 3 weeks".format(selected_species))
 plt.xlabel("Week")
 plt.ylabel("Length in inches")
 st.pyplot(fig)
+
+sidebar.make()
